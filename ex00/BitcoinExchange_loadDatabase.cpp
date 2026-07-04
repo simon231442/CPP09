176359be71de348c3ec0698b7a3b07101a986481
simon231442 <simon2314@hotmail.com>
2026-07-04 18:44:56 +0200

    lenovo  : ex00 : loaddatabase suite2

--- ex00/BitcoinExchange_loadDatabase.cpp
+++ ex00/BitcoinExchange_loadDatabase.cpp
@@ -1,15 +1,16 @@
 #include "BitcoinExchange.hpp"
 #include <fstream>
 #include <stdexcept>
+#include <cstdlib>
 
 void BitcoinExchange::loadDatabase(std::string const & databaseFile) 
 {
 	std::string		line;
 	std::string		date;
 	std::string		priceStr;
-	flaot			price;
+	float			price;
 
-	std::ifstream	file(databaseFile.str());
+	std::ifstream	file(databaseFile.c_str());
 	if (!file.is_open())
 		throw std::runtime_error("Couldn't open the database file");
 	
@@ -18,18 +19,21 @@ void BitcoinExchange::loadDatabase(std::string const & databaseFile)
 	while(std::getline(file, line))
 	{
 		size_t	commaPos = line.find(',');
-		if (commaPos == std::string::nopos)
+		if (commaPos == std::string::npos)
 			continue;
 		date = line.substr(0, commaPos);
 		priceStr = line.substr(commaPos + 1);
 		date = trim(date);
-		priceStr = trim(priceStr;
+		priceStr = trim(priceStr);
 
 		try {
-			price = std::stof(priceStr);
+			price = std::strtof(priceStr.c_str(), NULL);
 			this->rates_[date] = price;
 		}
-		catch {
+		catch (std::exception const &e) {
 			continue ;
+		}
+	}
+}
 
 
