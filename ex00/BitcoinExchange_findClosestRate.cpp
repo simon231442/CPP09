d8521e26cdf9b18694730351abd5a1e69f08824d
simon231442 <simon2314@hotmail.com>
2026-07-06 17:17:47 +0200

    lenovo : ex00 : findclosest ok , printconversion bientot

--- ex00/BitcoinExchange_findClosestRate.cpp
+++ ex00/BitcoinExchange_findClosestRate.cpp
@@ -1,7 +1,16 @@
 #include "BitcoinExchange.hpp"
 
-BitcoinExchange::PriceDataBase::const_iterator	BitcoinExchange::findClosestRate(std::string const & date) const
+BitcoinExchange::PriceDatabase::const_iterator	BitcoinExchange::findClosestRate(std::string const & date) const
 {
+	std::string						normalizedDate = this->normalizeDate(date);
+	PriceDatabase::const_iterator	it = this->rates_.lower_bound(normalizedDate);
 
+	if (it != this->rates_.begin() && it == this->rates_.end())
+	{
+		if (it->first != normalizedDate)
+			--it;
+	}
 
+	return it;
+}
 
--- ex00/BitcoinExchange_printConversion.cpp
+++ ex00/BitcoinExchange_printConversion.cpp
@@ -6,4 +6,12 @@ void		BitcoinExchange::printConversion(std::string const & date, float value) co
 	float							rate;
 
 	it = findClosestRate(date);
+	if (this->rates_.end() == it && it->first != this->normalizeDate(date))
+	{
+		std::cerr << "Error: no rate aviable for the date " << date << std::endl;
+		return ;
+	}
+
+	std::cout << date << " => " << value << " = " << value * rate;
+}
 	
