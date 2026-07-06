b2b61a69ef666826af9a8d4d353b2635439bbc31
simon231442 <simon2314@hotmail.com>
2026-07-06 11:27:39 +0200

    lenovo : ex00 : valueisvalide suite3

--- ex00/BitcoinExchange_valueIsValid.cpp
+++ ex00/BitcoinExchange_valueIsValid.cpp
@@ -1,4 +1,31 @@
 #include "BitcoinExchange.hpp"
 
-bool	BitcoinExchange::ValueIsValid(std::string const & Value) const
+bool	BitcoinExchange::ValueIsValid(std::string const & value) const
 {
+	size_t		i = 0;
+	bool		hasDecimal = false;
+	char*		end;
+
+	if (value.is_empty())
+		return false;
+	
+	if (value[i] == '-')
+		return false;
+
+	for (; i < value.size(); ++i)
+	{
+		if (value[i] == '.')
+		{
+			if (hasDecimal)
+			{
+				return false;
+				hasDecimal = true;
+			}
+		}
+		else if (!isdigit(value[i]))
+			return false;
+	}
+
+	if (hasDecimal && (value[0] == '.' || value[value.size() -1]))
+		return false;
+	
