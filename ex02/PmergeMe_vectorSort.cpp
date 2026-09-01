ae48b40474d23eab5409889f8a39ecdb0ea5e931
Simon Renaud <srenaud@c3r5s3.42lausanne.ch>
2026-09-01 20:26:06 +0200

    ssd : ex02 : vectorSort : emellage de pinceau avec pend..

--- ex02/PmergeMe_vectorSort.cpp
+++ ex02/PmergeMe_vectorSort.cpp
@@ -1,9 +1,40 @@
 #include "PmergeMe.hpp"
 
+static int	oddCaseHandle(std::vector<int> & container);
+static std::vector<std::pair<int, int> >	pairsMake(std::vector<int> const & container);
 
 void PmergeMe::vectorSort(std::vector<int>& container)
 {
-    if (container.size() <= 1)
-        return;
+	if (container.size() <= 1)
+		return;
 
+	int	oddOne = oddCaseHandle(container);
+	std::vector<std::pair<int, int> >	pairs = pairsMake(container);
 
+	(void)oddOne;
+	(void)pairs;
+	// TODO : extraction des grands, recursion, reattach, jacobsthal, insertion
+}
+
+static int	oddCaseHandle(std::vector<int> & container)
+{
+	if (container.size() % 2 == 0)
+		return -1;
+	int	oddOne = container.back();
+	container.pop_back();
+	return oddOne;
+}
+
+static std::vector<std::pair<int, int> >	pairsMake(std::vector<int> const & container)
+{
+	std::vector<std::pair<int, int> >	pairs;
+
+	for (size_t i = 0; i < container.size(); i += 2)
+	{
+		if (container[i] > container[i + 1])
+			pairs.push_back(std::make_pair(container[i], container[i + 1]));
+		else
+			pairs.push_back(std::make_pair(container[i + 1], container[i]));
+	}
+	return pairs;
+}
